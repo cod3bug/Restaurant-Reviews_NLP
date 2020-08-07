5fc59394c81d3a7d319b93360bfde399b05f7507
Sarthak Gupta <sarthakg203@gmail.com>
2020-08-07 14:16:49 +0530

    Second commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
+import os
 filename = 'nlp_model.pkl'
 classifier = pickle.load(open(filename, 'rb'))
 cv = pickle.load(open('transform.pkl', 'rb'))
@@ -14,5 +15,7 @@ def predict():
     vect = cv.transform(data).toarray()
     my_pred = classifier.predict(vect)
     return render_template("index.html", prediction = "Positive" if my_pred else "Negative")
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
